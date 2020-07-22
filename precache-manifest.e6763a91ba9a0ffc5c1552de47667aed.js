86f6a93502372100625a4d1d8f8464c978e66014
SlimTim10 <slimtim10@gmail.com>
2020-07-22 12:22:30 -0400

    Updates

---
+++ precache-manifest.e6763a91ba9a0ffc5c1552de47667aed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d5bbae85626b0c03937b75158455e1e",
+    "url": "/colour-picker-mintbean-hackathon/index.html"
+  },
+  {
+    "revision": "2534edfcbb6c1a1c53f0",
+    "url": "/colour-picker-mintbean-hackathon/static/css/main.0675f0e3.chunk.css"
+  },
+  {
+    "revision": "07e00e82b775e5f6f5a7",
+    "url": "/colour-picker-mintbean-hackathon/static/js/2.f7ff3ccd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/colour-picker-mintbean-hackathon/static/js/2.f7ff3ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2534edfcbb6c1a1c53f0",
+    "url": "/colour-picker-mintbean-hackathon/static/js/main.49b1579f.chunk.js"
+  },
+  {
+    "revision": "18bce7a9936c29aeae6e",
+    "url": "/colour-picker-mintbean-hackathon/static/js/runtime-main.667e5161.js"
+  }
+]);
